9b92cb9ab5d7f38d21e6f1953c89a3a59be983ff
Mehmet Baykar <mehmetbaykar2015@gmail.com>
2020-04-28 00:48:55 +0200

    Update SwiftWebVCActivity.swift
    Update Swift 5

--- SwiftWebVC/SwiftWebVCActivity.swift
+++ SwiftWebVC/SwiftWebVCActivity.swift
@@ -10,16 +10,16 @@ import UIKit
 
 
 class SwiftWebVCActivity: UIActivity {
-
+    
     var URLToOpen: URL?
     var schemePrefix: String?
     
-    override var activityType : UIActivityType? {
+    override var activityType: UIActivity.ActivityType?{
         let typeArray = "\(type(of: self))".components(separatedBy: ".")
         let _type: String = typeArray[typeArray.count-1]
-        return UIActivityType(rawValue: _type)
+        return UIActivity.ActivityType(rawValue: _type)
     }
-        
+    
     override var activityImage : UIImage {
         if let type = activityType?.rawValue {
             return SwiftWebVC.bundledImage(named: "\(type)")!
@@ -29,7 +29,7 @@ class SwiftWebVCActivity: UIActivity {
             return UIImage()
         }
     }
-            
+    
     override func prepare(withActivityItems activityItems: [Any]) {
         for activityItem in activityItems {
             if activityItem is URL {
@@ -37,5 +37,5 @@ class SwiftWebVCActivity: UIActivity {
             }
         }
     }
-
+    
 }
